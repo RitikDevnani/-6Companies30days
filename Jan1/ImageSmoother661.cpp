171591424ce18d1f69fd775801a6f9e7b98bc2bd
Ritik Devnani <ritikdev.04@gmail.com>
2025-01-01 22:10:52 +0530

    added comments on ImageSmoother661 answer

--- Jan1/ImageSmoother661.cpp
+++ Jan1/ImageSmoother661.cpp
@@ -7,14 +7,16 @@ public:
         for(int i = 0; i < img.size(); i++)
         {
             for(int j = 0; j < img[0].size(); j++)
-            {
-                sum = 0;
-                n = 0;
+           {
+                //arrived on element img[x][y]
+
+                sum = 0; //total sum of neighbour elements including yourself
+                n = 0; //total number of neighbours including yourself
                 for(int x = i - 1; x < i + 2; x++)
                 {
                     for(int y = j - 1; y < j + 2; y++)
                     {
-                        if(x >= 0 && x < img.size() && y >= 0 && y < img[0].size())
+                        if(x >= 0 && x < img.size() && y >= 0 && y < img[0].size()) //checking if element exists or not
                         {
                             sum += img[x][y];
                             n++;
@@ -22,6 +24,7 @@ public:
                     }
                 }
 
+                //new element value
                 result[i][j] = floor(sum/n);
             }
         }
